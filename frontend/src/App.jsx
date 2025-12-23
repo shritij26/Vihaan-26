1ca1e0dfe23f4e6a8b0a32a461e3c58f8c26ba16
KaranGupta_2006 <guptakaran.port@gmail.com>
2025-12-23 09:35:02 +0530

    landing2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,42 @@
 import { useState } from 'react'
 import Landing from './components/Landing.jsx'
-<<<<<<< HEAD
 import Intro from './components/Loader.jsx'
 import Tracks from './components/Tracks.jsx'
-=======
 import Landing2 from './components/Landing2.jsx'
->>>>>>> main
 import './App.css'
 
 function App() {
- 
-   const [showIntro, setshowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(true)
 
   return (
     <>
-     {showIntro && (
-    <div id='loader'>
-      <Intro onComplete={() => {
-        console.log('Intro complete!');
-        setshowIntro(false)}} />
-    </div>
-     )}
-     {!showIntro && (
-    <div id='landing'>
-      <Landing></Landing>
-    </div>
-<<<<<<< HEAD
-    
-     )}
-     <div id='tracks'>
-      <Tracks></Tracks>
-=======
-    <div id='landing2'>
-      <Landing2></Landing2>
->>>>>>> main
-    </div>
+      {showIntro && (
+        <div id="loader">
+          <Intro
+            onComplete={() => {
+              console.log('Intro complete!')
+              setShowIntro(false)
+            }}
+          />
+        </div>
+      )}
+
+      {!showIntro && (
+        <div id="landing">
+          <Landing />
+        </div>
+      )}
+
+      <div id="tracks">
+        <Tracks />
+      </div>
+
+      <div id="landing2">
+        <Landing2 />
+      </div>
     </>
   )
 }
 
 export default App
+
