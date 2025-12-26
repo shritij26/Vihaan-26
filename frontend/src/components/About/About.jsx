20e62435e1d9bc48defbadc2f371a9dd60fd058b
Rahul69-aiiy <prataprahul68@gmail.com>
2025-12-26 13:31:56 +0530

    about us

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import "./About.css"
+import Header from './Header'
+import Description from './Description'
+import Alumni from './Alumni'
+import Contacts from './Contacts'
+import Socials from './Socials'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='About'>
+      <Header />
+      <Description />
+      <Alumni />
+      <Contacts />
+      <Socials />
+    </div>
   )
 }
+
